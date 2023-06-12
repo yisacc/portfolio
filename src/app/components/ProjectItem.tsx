7ece4786a6df1bd537719da41e0a9980a1098453
Yisacc Aberham <isaccab2019@gmail.com>
2023-06-12 13:49:42 +0300

    moved the projects to a config file

--- src/app/components/ProjectItem.tsx
+++ src/app/components/ProjectItem.tsx
@@ -1,40 +1,43 @@
+import { ProjectItems } from "@/lib/projects";
 import Image, { StaticImageData } from "next/image";
-
-interface ProjectItemProps {
-  title: string;
-  description: string;
-  technologies: string[];
-  imageSrc: StaticImageData;
-}
 
 const ProjectItem = ({
   title,
+  subTitle,
   description,
   technologies,
   imageSrc,
-}: ProjectItemProps) => {
+  className,
+  imageClass,
+  listClass,
+}: ProjectItems) => {
   return (
     <article className="sm:grid sm:grid-cols-10 gap-4">
-      <div className="order-2 text-right sm:col-start-5 sm:col-end-11 sm:row-start-1 sm:row-end-2">
+      <div className={`order-2 sm:row-start-1 sm:row-end-2 ${className}`}>
         <h4>{title}</h4>
-        <h3 className="text-aqua text-2xl font-bold">{description}</h3>
-        <p className="bg-black p-4 rounded-lg text-base text-white leading-6 text-left">
-          {description}
-        </p>
+        <h3 className="text-aqua text-2xl font-bold">{subTitle}</h3>
+        <p
+          className="bg-black p-4 rounded-lg text-base text-white leading-6 text-left"
+          dangerouslySetInnerHTML={{ __html: description }}
+        ></p>
         <h4 className="font-mono text-base m-0">Technologies used include:</h4>
-        <ul className="list-none p-0 m-0 flex flex-row flex-wrap justify-start gap-4 text-base sm:justify-end">
+
+        <ul
+          className={`list-none p-0 m-0 flex flex-row flex-wrap justify-start gap-4 text-base ${listClass}`}
+        >
           {technologies.map((tech, index) => (
             <li key={index}>{tech}</li>
           ))}
         </ul>
       </div>
-
-      <div className="mt-8 mx-0 mb-16 border-l border-aqua border-solid border-t rounded-3xl p-4 sm:col-start-1 sm:col-end-6 sm:row-start-1 sm:row-end-2">
+      <div
+        className={`mt-8 mx-0 mb-16 border-l border-aqua border-solid border-t rounded-3xl p-4 sm:row-start-1 sm:row-end-2 ${imageClass}`}
+      >
         <Image
           width={584}
           height={358}
           src={imageSrc}
-          alt={`Screenshot of ${title}`}
+          alt={`Screenshot of ${subTitle}`}
         />
       </div>
     </article>
--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -3,23 +3,48 @@ import assemblyConnectPic from "../../public/assembly connect.png";
 import fusionPic from "../../public/fusion.png";
 import { StaticImageData } from "next/image";
 
-export interface Project {
+export interface ProjectItems {
   title: string;
+  subTitle: string;
   description: string;
   technologies: string[];
   imageSrc: StaticImageData;
+  className: string;
+  imageClass: string;
+  listClass: string;
 }
 
-export const projects:Project[] = [
+export const projects: ProjectItems[] = [
   {
-    title: "dHealth Signer app",
-    description: "dHealth Signer app",
+    title: "Latest Project",
+    subTitle: "dHealth Signer app",
+    description: `The dHealth signer app is a non custodial client (wallet) that
+    allows users to interact with dApps in the dHealth Ecosystem and
+    view and maintain your DHP balance. The 
+    <a href='https://www.dhealth.com/' target="_blank">
+      dHealth signer app
+    </a>
+    additionally supports signing of all dHealth transactions.p`,
     technologies: ["React Native", "Typescript", "Blockchain", "CSS"],
     imageSrc: signerAppPic,
+    className: "text-right sm:col-start-5 sm:col-end-11 ",
+    imageClass: "sm:col-start-1 sm:col-end-6",
+    listClass: "sm:justify-end",
   },
   {
-    title: "Assemble Connect Tenant Portal",
-    description: "Assemble Connect Tenant Portal",
+    title: "WAVEOC Project",
+    subTitle: "Assemble Connect Tenant Portal",
+    description: `              <a
+    href="https://myaccount.assembleconnect.com.au/login"
+    target="_blank"
+  >
+    Assemble connect
+  </a>
+   provides utility services (hot water, electricity, and internet)
+  to apartment tenants. Tenants can use the tenant portal to
+  register, select a tariff plan, set up a payment method (bank
+  account, card, or direct debit), generate monthly and weekly
+  invoices, and disconnect the service.`,
     technologies: [
       "React",
       "Typescript",
@@ -29,11 +54,31 @@ export const projects:Project[] = [
       "Redux",
     ],
     imageSrc: assemblyConnectPic,
+    className: "text-left sm:col-start-1 sm:col-end-7 ",
+    imageClass: "sm:col-start-6 sm:col-end-11",
+    listClass: "sm:justify-start",
   },
   {
-    title: "Fusion web application",
-    description: "Fusion web application",
+    title: "Excellerent Project",
+    subTitle: "Fusion web application",
+    description: `The
+    <a
+      href="https://blackbox.blackboxintelligence.com/"
+      target="_blank"
+    >
+      Fusion
+    </a>
+    project bring together all of BBI core products
+    (Workforce,Financial, Guest) into one product offering so users
+    are capable of accessing all of BBI without having to log into
+    multiple applications. The efforts done throughout this project
+    will see an improved and modernized UI as well as an opportunity
+    for current BBI users to utilize a SSO service to access their
+    products.`,
     technologies: ["React", "Typescript", "Redux", ".Net", "CSS", "Azure"],
     imageSrc: fusionPic,
+    className: "text-right sm:col-start-5 sm:col-end-11 ",
+    imageClass: "sm:col-start-1 sm:col-end-6",
+    listClass: "sm:justify-end",
   },
 ];
