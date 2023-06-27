6b671045735c557ad57da988935ea659e8d8a625
Yisacc Aberham <isaccab2019@gmail.com>
2023-06-27 12:11:19 +0300

    fix build issue add vite config file

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import path from "node:path";
+import { defineConfig, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+const configuration: UserConfig = {
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      $components: path.resolve(__dirname, "./src/components"),
+    },
+  },
+};
+
+export default configuration;
