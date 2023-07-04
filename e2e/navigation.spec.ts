d63e307306487bee5100259ae77fe44152bb8ee7
Yisacc Aberham <isaccab2019@gmail.com>
2023-07-04 13:51:29 +0300

    fix playwright failing tests

--- e2e/navigation.spec.ts
+++ e2e/navigation.spec.ts
@@ -5,7 +5,7 @@ test('test', async ({ page }) => {
   await page.getByRole("link", { name: "Yisacc Aberham" }).click();
   await page.getByRole("link", { name: "Projects" }).click();
   await page.getByRole("link", { name: "About" }).click();
-  await page.goto("/about");
+  // await page.goto("/about");
   await page.getByRole("heading", { name: "About Yisacc Aberham" }).click();
   await page
     .getByRole("img", { name: "Screenshot of the dHealth signer app." })
@@ -15,19 +15,21 @@ test('test', async ({ page }) => {
       "I am a software engineer with 5 years of experience in the software industry. My"
     )
     .click();
-    await page.getByRole("link", { name: "Yisacc Aberham" }).click();
-    await page.goto("/");
   await page.getByRole("heading", { name: "Just the highlights" }).click();
+
   await page
     .getByRole("heading", { name: "My favorite technologies include:" })
     .click();
   await page
     .getByText("ReactReact NativeTypescriptNext.jsNode.NETGraphQL...")
     .click();
+
   await page.getByRole("link", { name: "Yisacc Aberham" }).click();
   await page.getByText("Yisacc Aberham.", { exact: true }).click();
-  await page.getByRole('heading', { name: 'I\'m a front-end developer.' }).click();
-  await page.getByRole('heading', { name: 'Projects I\'m proud of' }).click();
+  await page
+    .getByRole("heading", { name: "I'm a front-end developer." })
+    .click();
+  await page.getByRole("heading", { name: "Projects I'm proud of" }).click();
   await page.getByTestId("contact-me").click();
   await page.getByRole("link", { name: "email me" }).click();
 });
